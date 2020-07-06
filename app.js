61e443b9427c2a5e53cff00ced27459500e0b315
heyimmatt <mrmcfarland@gmail.com>
2020-07-06 10:23:57 -0700

    Complete parts 2-4 of the exercise

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 $(function () {
   console.log('Let\'s get ready to party with jQuery');
+  $('article > img').addClass('image-center');
+  $('article :last-child').remove();
+  $('#title').css('font-size', Math.floor(Math.random() * 100 + 1))
 })
