536bf574f9c920734c9c762fb69684dd3a83da75
heyimmatt <mrmcfarland@gmail.com>
2020-07-06 11:39:59 -0700

    Clear inputs on add movie and click listener to remove movie entries when button is clicked

--- jquery-movies-app/app.js
+++ jquery-movies-app/app.js
@@ -1,8 +1,8 @@
 $(function () {
   $('form').submit(function (evt) {
     evt.preventDefault();
-    const title = $('#title').val();
-    const rating = parseInt($('#rating').val());
+    let title = $('#title').val();
+    let rating = parseInt($('#rating').val());
 
     if (title.length > 2 && rating > 0 && rating <= 10) {
       let $movieEntry = $('<tr>').html(`
@@ -10,6 +10,15 @@ $(function () {
       <td>${rating}</td>
       <td><button class="btn btn-sm btn-danger">Remove</button></td>`);
       $('tbody').append($movieEntry);
+
+      $('#title').val('');
+      $('#rating').val('');
+
     } else alert('Movie title must be at least 2 characters and rating must be between 1 and 10');
   });
+
+  $('tbody').on('click', 'tr td button', function() {
+    $(this).parent().parent().remove();
+  })
+
 });
