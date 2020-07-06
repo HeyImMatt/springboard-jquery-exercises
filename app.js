becd582e5143d29ec1fc539bc80f78a79b8bf14e
heyimmatt <mrmcfarland@gmail.com>
2020-07-06 10:40:44 -0700

    Complete parts 5-8 of the exercise

--- app.js
+++ app.js
@@ -2,5 +2,16 @@ $(function () {
   console.log('Let\'s get ready to party with jQuery');
   $('article > img').addClass('image-center');
   $('article :last-child').remove();
-  $('#title').css('font-size', Math.floor(Math.random() * 100 + 1))
+  $('#title').css('font-size', Math.floor(Math.random() * 100 + 1));
+  $('ol').append('<li>It\'s in the cup!</li>')
+  $('aside').empty().append('<p>Sorry for the existence or non-existence of this list</p>');
+  $('input').on('change', function() {
+    let r = $('input').eq(0).val();
+    let g = $('input').eq(1).val();
+    let b = $('input').eq(2).val();
+    $('body').css('background-color', `rgb(${r}, ${g}, ${b})`);
+  })
+  $('img').click(function() {
+    $(this).remove();
+  })
 })
